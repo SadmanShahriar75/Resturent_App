51eb8bb17671dcd550b4eba5a5cc7764a0596e1a
SadmanShahriar75 <sadmanshahriar7576@gmail.com>
2024-02-13 22:31:31 +0600

    first commit

--- lib/app/routes/app_pages.dart
+++ lib/app/routes/app_pages.dart
@@ -20,7 +20,7 @@ part 'app_routes.dart';
 class AppPages {
   AppPages._();
 
-  static const INITIAL = Routes.BOTTOM_NAV_BAR;
+  static const INITIAL = Routes.ONBOARDING;
 
   static final routes = [
     GetPage(
